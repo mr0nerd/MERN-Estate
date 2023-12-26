4a5789e394ac6d983257c5a59c5942290a01a599
mr0nerd <adpcmecs1492@gmail.com>
2023-12-26 14:43:14 +0530

    Third commit

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
-import authrouter from './routes/auth.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -22,7 +23,8 @@ app.listen(process.env.PORT,()=>{
 })
 
 app.use('/api/user',userRouter);
-app.use('/api/auth',authrouter);
+app.use('/api/auth',authRouter);
+app.use('/api/listing',listingRouter);
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,10 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import Header from './Components/Header'
+import PrivateRoute from './Components/PrivateRoute'
+import CreateListing from './pages/CreateListing'
+import UpdateListing from './pages/UpdateListing'
+import Listing from './pages/Listing'
 
 const App = () => {
   return (
@@ -17,7 +21,12 @@ const App = () => {
       <Route path='/sign-in' element={<SignIn/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
       <Route path='/about' element={<About/>}/>
+      <Route path='/listing/:listingId' element={<Listing />} />
+      <Route element={<PrivateRoute/>}>
       <Route path='/profile' element={<Profile/>}/>
+      <Route path='/create-listing' element={<CreateListing/>}/>
+      <Route path='/update-listing/:listingId' element={<UpdateListing/>}/>
+      </Route>
     </Routes>
     </BrowserRouter>
   )
